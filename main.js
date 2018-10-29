3e312d4330b1e0123eecca5f5e8044e45c0a932e
Akash Agarwal <akashaggarwal777@gmail.com>
2018-10-30 08:24:32 +1100

    CHANGE: Main window size

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 900, height: 600})
+  mainWindow = new BrowserWindow({width: 900, height: 650})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
